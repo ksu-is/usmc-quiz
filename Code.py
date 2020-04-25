a1e1bab47d714308a0670a5484564aa99857c1de
amberrwalshh <59628733+amberrwalshh@users.noreply.github.com>
2020-04-24 20:13:34 -0400

    code
    changed code format

--- Code.py
+++ Code.py
@@ -1,63 +1,55 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, session
 
-app = Flask(QUIZ)
+app=Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
     return render_template('index-quizgame.html')
 
-@app.route('/py', methods=['GET', 'POST'])
-
+@app.route('/quiz',methods=['GET','POST'])
 def quiz():
+    
+    
 
-    character_names = open('characters.txt','r')
+character_names = open('characters.txt','r')
+characters = character_names.readline()
+
+while characters:
     characters = character_names.readline()
-
-    while characters:
-        characters = character_names.readline()
     
-    if request.method == "POST":
+correct = []
+incorrect = []
+character_list = []
 
-        input01 = request.form['input01'].capitalize()
-        input02 = request.form['input02'].capitalize()
-        input03 = request.form['input03'].capitalize()
-        input04 = request.form['input04'].capitalize()
-        input05 = request.form['input05'].capitalize()
-        input06 = request.form['input06'].capitalize()
-        input07 = request.form['input07'].capitalize()
-        input08 = request.form['input08'].capitalize()
-        input09 = request.form['input09'].capitalize()
-        input10 = request.form['input10'].capitalize()
-        input11 = request.form['input11'].capitalize()
-        input12 = request.form['input12'].capitalize()
-        input13 = request.form['input13'].capitalize()
-        input14 = request.form['input14'].capitalize()
-        input15 = request.form['input15'].capitalize()
-        input16 = request.form['input16'].capitalize()
-        input17 = request.form['input17'].capitalize()
-        input18 = request.form['input18'].capitalize()
-        input19 = request.form['input19'].capitalize()
-        input20 = request.form['input20'].capitalize()
-        input21 = request.form['input21'].capitalize()
-        input22 = request.form['input22'].capitalize()
-        input23 = request.form['input23'].capitalize()
-        input24 = request.form['input24'].capitalize()
-        input25 = request.form['input25'].capitalize()
-        input26 = request.form['input26'].capitalize()
-        input27 = request.form['input27'].capitalize()
-        input28 = request.form['input28'].capitalize()
-        input29 = request.form['input29'].capitalize()
-        input30 = request.form['input30'].capitalize()
+def get_names():
+    count = 0
+    while count <= 30:
+        character_input = request.form['character_input']
+        if character_input.capitalize() in character_list:
+            print(character_input,"already entered into list. No duplicates.")
+        elif len(character_input) > 0:
+            character_list.append(character_input.capitalize())
+            count += 1
+        elif count > 30:
+            break
+    return character_list
 
-        correct = []
-        incorrect = []
-        input_names = []
-    
-        for name in characters:
-            if input01 in characters:
-                correct.append(name)
-            else:
-                incorrect.append(name)            
+get_names()
 
-if QUIZ == '__main__':
-    app.run(host = '0.0.0.0', port = 3000)
+for name in character_list:
+    if name in characters:
+        correct.append(name)
+    else:
+        incorrect.append(name)
+        
+correct_numbers = len(correct)
+
+grade = correct_numbers * 20
+
+
+print('\n')
+print("Character List:",character_list,"\n")
+print("Grade:",grade,"%")
+print("Correct Characters:",correct)
+print("Incorrect Characters:",incorrect)
